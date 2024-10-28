b9c7b5f9f34aff7c8476c3d5d7d93a98db2e2425
Sevak Mazmanian <mazmaniansevak@gmail.com>
2024-10-28 19:59:40 +0200

    first update

---
+++ Web_Practice/Javascript/To_do_List/script.js
@@ -0,0 +1,35 @@
+const inputValue = document.querySelector('.description');
+const btn = document.querySelector('#add');
+
+btn.addEventListener('click', () => {
+  if (inputValue.value.trim()) {
+    buildTask(inputValue.value.trim());
+    inputValue.value = '';
+  } else {
+    alert("Please enter a task.");
+  }
+});
+
+function buildTask(task) {
+  const taskDiv = document.createElement('div');
+  taskDiv.className = 'task-container';
+  const input = document.createElement('input');
+  input.className = 'to-do';
+  input.type = 'Text';
+  input.value = task;
+  input.readOnly = true;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.className = 'del';
+
+  deleteBtn.addEventListener('click', () => {
+    taskContainer.remove();
+  });
+
+  taskDiv.appendChild(input);
+  taskDiv.appendChild(deleteBtn);
+
+  const taskContainer = document.querySelector('.show-tasks');
+  taskContainer.appendChild(taskDiv);
+}
